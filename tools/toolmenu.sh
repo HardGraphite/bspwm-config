af93cf3c86be12a5510bbe14b9dbf379279b3f17
HardGraphite <jodieydchen@gmail.com>
2024-02-15 14:12:34 +0800

    Add compositor control switch

--- tools/toolmenu.sh
+++ tools/toolmenu.sh
@@ -4,7 +4,9 @@ declare -A tools=(
     ['USB Storage Devices Control']='udisksctl.sh'
     ['Power Menu']='power.sh'
     ['Lock Screen']='lockscr.sh'
+    ['Compositor: start']='compositor.sh start'
+    ['Compositor: stop']='compositor.sh stop'
 )
 
-script=$(dirname $(readlink -f "$0"))/${tools[$(printf '%s\n' "${!tools[@]}" | rofi -no-plugins -dmenu -i -p 'Tool')]}
-[ -f "$script" ] && exec "$script"
+script=$(dirname $(readlink -f "$0"))/${tools[$(printf '%s\n' "${!tools[@]}" | rofi -no-plugins -no-sort -dmenu -i -p 'Tool')]}
+exec $script
